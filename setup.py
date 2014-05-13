a24a86885c1569612ff70d65502768f8043262be
Kyle Maroney <kmaroney@rethinkrobotics.com>
2014-05-13 17:27:39 -0400

    PyKDL toying with parsing dynamic Baxter URDF.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['baxter_haptics']
+d['packages'] = ['baxter_haptics', 'baxter_kdl',
+                 'urdf_parser_py',]
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
